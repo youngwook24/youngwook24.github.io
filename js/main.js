b9968e6d87a9142fa6685622b849f3e7395fb98c
youngwook24 <daniel.ywjung@gmail.com>
2023-05-02 20:23:54 -0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -428,7 +428,7 @@
             zoom: 16 // Google Map ZOOM. You can change this value
         });
         $("#map").addMarker({
-            address: "S601 Townsend Street, San Francisco, California, USA", // Your Address. Change it
+            address: "1407 W Gregory Dr, Urbana, Illinois, USA", // Your Address. Change it
         });
 
         scrollTop();
